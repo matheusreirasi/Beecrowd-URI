bb57811107176c109c3c83acd6ab412a7697d427
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-04 00:44:37 -0300

    Não entregue

--- 2449-fechadura.py
+++ 2449-fechadura.py
@@ -1,2 +1,12 @@
-pinos = int(input())
-altura = int(input())
+nPinos , altura = map(int , input().split())
+
+pinos= input().split()
+print(pinos)
+
+for i in range(0,len(pinos),2):
+    while pinos[i]!= altura:
+        for j in range()
+        if pinos[i] < altura:
+            pinos[i]+= pinos[i]
+        if pinos[i] > altura:
+            pinos[i]-= pinos[i]
