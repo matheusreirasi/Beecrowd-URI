8d4e0c967f3b8f1bfcc1c962638481d94a88872c
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-04 13:31:40 -0300

    Time limit exceeded

--- 1259-parimpar.py
+++ 1259-parimpar.py
@@ -9,15 +9,12 @@ for i in range(N):
         p.append(v[i])
     if v[i]%2!=0:
         im.append(v[i])
-        
+
 for i in range(len(p)):
-    for j in range(len(p)-1):
-        if p[j] > p[j+1]:
-            p[j],p[j+1] = p[j+1],p[j]
+    p.sort()
 for i in range(len(im)):
-    for j in range(len(im)-1):
-        if im[j] < im[j+1]:
-            im[j],im[j+1] = im[j+1],im[j]
+    im.sort(reverse=True)
+
 print(p+im)
 
 #DEU TIME LIMIT NO BEECROWD
