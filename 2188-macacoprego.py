f0a195ec98a56b4ab359c90ec16a45611149a179
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-05 00:27:09 -0300

    Não entregue

--- 2188-macacoprego.py
+++ 2188-macacoprego.py
@@ -1,7 +1,6 @@
-região=int(input())
+nRegiões=int(input())
 
-while região!=0:
-    for i in range(região):
-        x1 , y1 , x2 , y2 = input().split()
-        distancia = (abs(int(x2)) - abs(int(x1)) + abs(int(y2)) - abs(int(y1)))
-print(distancia)
+while nRegiões!=0:
+    x1 , y1 , x2 , y2 = input().split()
+        
+print(x1,y1,x2,y2)
