429854051a999cabf2bd918771a1e76726de0c0b
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-04 21:18:03 -0300

    Não entregue

--- 2449-fechadura.py
+++ 2449-fechadura.py
@@ -1,12 +1,22 @@
 nPinos , altura = map(int , input().split())
 
-pinos= input().split()
-print(pinos)
+pinos= map(int , input().split())
+movimentos=0
+#print(pinos)
 
-for i in range(0,len(pinos),2):
-    while pinos[i]!= altura:
-        for j in range()
-        if pinos[i] < altura:
-            pinos[i]+= pinos[i]
-        if pinos[i] > altura:
-            pinos[i]-= pinos[i]
+#for i in range(0,len(pinos),2):
+#    while pinos[i]!= altura:
+#        for j in range():
+#        if pinos[i] < altura:
+#            pinos[i]+= pinos[i]
+#        if pinos[i] > altura:
+#            pinos[i]-= pinos[i]
+
+
+for i in range(len(pinos)):
+    while pinos[i] or pinos[i+1] != altura:
+        if pinos[i] == altura:
+            pinos[i+1] = pinos[i]
+        if pinos[i] and pinos[i+1] != altura:
+            movimentos+=movimentos
+print(movimentos)
