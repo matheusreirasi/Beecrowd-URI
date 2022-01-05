04c5c35b464849523a7f510ce793a451d3495391
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-05 20:23:28 -0300

    Não entregue

--- 2188-macacoprego.py
+++ 2188-macacoprego.py
@@ -1,7 +1,21 @@
 nRegiões=int(input())
-região=[]*nRegiões
+xx=-10000
+yy=10000
+uu=10000
+vv=-10000
+teste=0
 
-for i in range(nRegiões):
-    x , y , u , v = (input().split()).append(região)
-
-print(região)
+while nRegiões != 0:
+    for i in range(nRegiões):
+        x , y , u , v = map(int, (input().split()))
+        if x > xx and x < uu:
+            xx=x
+        if y > vv and y < yy:
+            yy=y
+        if u > xx and u < uu:
+            uu=u
+        if v < yy and v > vv:
+            vv=v
+    teste+=1
+    print("Teste ",teste)
+    print(xx,yy,uu,vv)
