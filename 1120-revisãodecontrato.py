cf201345767e97e2ca37f41503d37f0486ff37c5
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-19 13:45:45 -0300

    Entregue

--- 1120-revisãodecontrato.py
+++ 1120-revisãodecontrato.py
@@ -1,7 +1,7 @@
 while True:
   dígito,valor=map(str,input().split())
 
-  if dígito == '0' and valor == '0':
+  if dígito == "0" and valor == "0":
     break
   aux = 0
   valor = list(valor)
@@ -17,15 +17,17 @@ while True:
     if aux == 0:
       if i != 0:
         lista_final.append(i)
-        aux+=1
+        aux+= 1
     else:
       lista_final.append(i)
-      aux+=1
+      aux+= 1
       
-
-  if sum(lista)==0:
+  soma = 0
+  for i in range(len(lista)):
+    soma+= lista[i]
+  if soma == 0:
     print("0")
-  else:  
+  else:
     for i in lista_final:
-      print(i,end=" ".strip())
+      print(i,end= "".strip())
     print()
