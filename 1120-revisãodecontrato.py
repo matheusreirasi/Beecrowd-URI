fdb095d4532b1de16557bd2cc85e7fa54adb9cdb
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-19 10:24:16 -0300

    Não entregue

--- 1120-revisãodecontrato.py
+++ 1120-revisãodecontrato.py
@@ -1,1 +1,9 @@
-asdf
+digito, número = list(map(int, input().split()))
+lista = []
+valor = []
+valor.append(número)
+
+for i in range(len(valor)):
+  if valor[i] != digito:
+    lista.append(valor[i])
+print(lista)
