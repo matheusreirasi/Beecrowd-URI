66dd70d451fb0f8fa31f186274d787e89232b787
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-19 21:04:26 -0300

    Entregue

--- 3102-kikoho.py
+++ 3102-kikoho.py
@@ -1,1 +1,7 @@
-asdf
+casos = int(input())
+
+while casos >= 1:
+    x1,y1,x2,y2,x3,y3 = map(int,input().split())
+    area = abs((0.5)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
+    casos -= 1
+    print("%.3f"%area)
